1be65cf8d0a516faaefecb16ff39c1f3dc14de04
Andrey Ribeiro <andrey_ribeiro96@hotmail.com>
2021-08-27 22:52:16 -0300

    Correcoes no carregamento do dataset

--- main.py
+++ main.py
@@ -4,32 +4,33 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout
 from keras.utils import np_utils
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
+#from keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 
 def main():
     # loading dataset
     (X_training, y_training), (X_test, y_test) = cifar100.load_data()
-    plt.imshow(X_training[1], cmap = 'gray') # Show image, cmap = gray remove the color of the image
-    plt.title('Class ' + str(y_training[1])) # Class of the image
+    plt.imshow(X_training[0]) # Show image, cmap = gray remove the color of the image
+    plt.title('Class ' + str(y_training[0])) # Class of the image
 
-    predictors_training = X_training.reshape(3, 32, 32, 1)
+    predictors_training = X_training.reshape(X_training.shape[0], 32, 32, 3)
 
-    predictors_test = X_test.reshape(3, 32, 32, 1)
+    predictors_test = X_test.reshape(X_test.shape[0], 32, 32, 3)
     predictors_training = predictors_training.astype('float32')
     predictors_test = predictors_test.astype('float32')
 
     predictors_training /= 255
     predictors_test /= 255
 
-    class_training = np_utils.to_categorical(y_treinamento, 100)
+    class_training = np_utils.to_categorical(y_training, 100)
     class_test = np_utils.to_categorical(y_test, 100)
 
     #plt.show()
 
     # Creating classifier
     classifier = Sequential()
-    classifier.add(Conv2D(32, (3,3), input_shape=(28, 28, 1),
-                   activation = 'relu')) #recommended start with 64 kernels instead of 32
+    classifier.add(Conv2D(32, (3,3), input_shape=(32, 32, 3),
+                   activation = 'relu')) #recommended start with 64 kernels instead of 32 input_shape=(28, 28, 1)
 
     classifier.add(BatchNormalization())
 
@@ -59,11 +60,16 @@ def main():
     classifier.add(Dense(units = 10, activation = 'softmax')) #output layer
     classifier.compile(loss = 'categorical_crossentropy',
                        optimizer = 'adam', metrics = ['accuracy'])
+
+    predictors_training = np_utils.to_categorical(predictors_training)
+
     classifier.fit(predictors_training, class_training,
                    batch_size = 128, epochs = 5,
                    validation_data = (predictors_test, class_test))
 
     result = classifier.evaluate(predictors_test, class_test)
+    print(result)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
