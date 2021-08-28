74d789fce741862d31c51e5ebf863c812808e7d9
Andrey Ribeiro <andrey_ribeiro96@hotmail.com>
2021-08-28 16:38:56 -0300

    validacao cruzada

--- main.py
+++ main.py
@@ -6,6 +6,43 @@ from keras.utils import np_utils
 from keras.layers import Conv2D, MaxPooling2D
 #from keras.layers.normalization import BatchNormalization
 from tensorflow.keras.layers import BatchNormalization
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+
+def CrossValidation():
+    seed = 5
+    np.random.seed(seed)
+
+    (X, y), (X_teste, y_teste) = cifar100.load_data()
+    predictors = X.reshape(X.shape[0], 32, 32, 3)
+    predictors = predictors.astype('float32')
+    predictors /= 255
+    classes = np_utils.to_categorical(y, 100)
+
+    kfold = StratifiedKFold(n_splits = 5, shuffle = True, random_state = seed)
+    results = []
+
+    a = np.zeros(5)
+    b = np.zeros(shape = (classes.shape[0], 1))
+
+    for training_index, test_index in kfold.split(predictors,
+                                                  np.zeros(shape = (classes.shape[0], 1))):
+        #print('Training indexes: ', training_index, 'Test indexes ', test_index)
+        classifier = Sequential()
+        classifier.add(Conv2D(32, (3,3), input_shape=(32, 32, 3),
+                       activation = 'relu')) #recommended start with 64 kernels instead of 32 input_shape=(28, 28, 1)
+        classifier.add(MaxPooling2D(pool_size = (2,2)))
+        classifier.add(Flatten())
+        classifier.add(Dense(units = 128, activation = 'relu'))
+        classifier.add(Dense(units = 10, activation = 'softmax')) #hidden layer
+        classifier.compile(loss = 'categorical_crossentropy',
+                           optimizer = 'adam', metrics = ['accuracy'])
+        classifier.fit(predictors[training_index], classes[training_index],
+                       batch_size = 128, epochs = 5)
+        precision = classifier.evaluate(predictors[test_index], classes[test_index])
+        results.append(precision[1])
+
+    mean = sum(results) / len(results)
 
 def main():
     # loading dataset
