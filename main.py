5636ec5dc50212ed3b301594053e212986bd81fe
Andrey Ribeiro <andrey_ribeiro96@hotmail.com>
2021-08-29 17:54:50 -0300

    mudanca da base de dados

--- main.py
+++ main.py
@@ -1,114 +1,153 @@
-import matplotlib.pyplot as plt
-from keras.datasets import cifar100
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
-from keras.utils import np_utils
-from keras.layers import Conv2D, MaxPooling2D
-#from keras.layers.normalization import BatchNormalization
-from tensorflow.keras.layers import BatchNormalization
 import numpy as np
-from sklearn.model_selection import StratifiedKFold
-
-def CrossValidation():
-    seed = 5
-    np.random.seed(seed)
-
-    (X, y), (X_teste, y_teste) = cifar100.load_data()
-    predictors = X.reshape(X.shape[0], 32, 32, 3)
-    predictors = predictors.astype('float32')
-    predictors /= 255
-    classes = np_utils.to_categorical(y, 100)
-
-    kfold = StratifiedKFold(n_splits = 5, shuffle = True, random_state = seed)
-    results = []
-
-    a = np.zeros(5)
-    b = np.zeros(shape = (classes.shape[0], 1))
-
-    for training_index, test_index in kfold.split(predictors,
-                                                  np.zeros(shape = (classes.shape[0], 1))):
-        #print('Training indexes: ', training_index, 'Test indexes ', test_index)
-        classifier = Sequential()
-        classifier.add(Conv2D(32, (3,3), input_shape=(32, 32, 3),
-                       activation = 'relu')) #recommended start with 64 kernels instead of 32 input_shape=(28, 28, 1)
-        classifier.add(MaxPooling2D(pool_size = (2,2)))
-        classifier.add(Flatten())
-        classifier.add(Dense(units = 128, activation = 'relu'))
-        classifier.add(Dense(units = 10, activation = 'softmax')) #hidden layer
-        classifier.compile(loss = 'categorical_crossentropy',
-                           optimizer = 'adam', metrics = ['accuracy'])
-        classifier.fit(predictors[training_index], classes[training_index],
-                       batch_size = 128, epochs = 5)
-        precision = classifier.evaluate(predictors[test_index], classes[test_index])
-        results.append(precision[1])
-
-    mean = sum(results) / len(results)
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import cv2
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+import random
+from numpy import argmax
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 def main():
-    # loading dataset
-    (X_training, y_training), (X_test, y_test) = cifar100.load_data()
-    plt.imshow(X_training[0]) # Show image, cmap = gray remove the color of the image
-    plt.title('Class ' + str(y_training[0])) # Class of the image
+    # path to images
+    path = 'C:/Users/andre/OneDrive/Documents/Aula/IA - Adriana/Aula8/Trabalho/animals-recognition/animals/animals/'
 
-    predictors_training = X_training.reshape(X_training.shape[0], 32, 32, 3)
+    # animal classes
+    classes = ['dogs', 'panda', 'cats']
 
-    predictors_test = X_test.reshape(X_test.shape[0], 32, 32, 3)
-    predictors_training = predictors_training.astype('float32')
-    predictors_test = predictors_test.astype('float32')
+    #1. Display pictures
+    for category in classes:
+    	fig, _ = plt.subplots(3,4)
+    	fig.suptitle(category)
+    	for k, v in enumerate(os.listdir(path+category)[:12]):
+    		img = plt.imread(path+category+'/'+v)
+    		plt.subplot(3, 4, k+1)
+    	plt.show()
 
-    predictors_training /= 255
-    predictors_test /= 255
+    #2. Pictures shape
+    shape0 = []
+    shape1 = []
 
-    class_training = np_utils.to_categorical(y_training, 100)
-    class_test = np_utils.to_categorical(y_test, 100)
+    for category in classes:
+    	for files in os.listdir(path+category):
+    		shape0.append(plt.imread(path+category+'/'+ files).shape[0])
+    		shape1.append(plt.imread(path+category+'/'+ files).shape[1])
 
-    #plt.show()
+    	print(category, ' => height min : ', min(shape0), 'width min : ', min(shape1))
+    	print(category, ' => height min : ', min(shape0), 'width min : ', min(shape1))
+    	shape0 = []
+    	shape1 = []
+
+    #3. Preprocess data and label inputs
+
+    data = []
+    labels = []
+    imagePaths = []
+    HEIGHT = 32
+    WIDTH = 55
+    N_CHANNELS = 3
+
+    for k, category in enumerate(classes):
+    	for f in os.listdir(path+category):
+    		imagePaths.append([path+category+'/'+f, k]) # k=0 : 'dogs', k=1 : 'panda', k=2 : 'cats'
+
+    random.shuffle(imagePaths)
+    print(imagePaths[:10])
+
+    for imagePath in imagePaths:
+    	image = cv2.imread(imagePath[0])
+    	image = cv2.resize(image, (WIDTH, HEIGHT)) # .flatten()
+    	data.append(image)
+    	label = imagePath[1]
+    	labels.append(label)
+
+
+    data = np.array(data, dtype="float") / 255.0
+    labels= np.array(labels)
+
+    plt.subplots(3,4)
+    for i in range(12):
+    	plt.subplot(3, 4, i+1)
+    	plt.imshow(data[i])
+    	plt.axis('off')
+    	plt.title(classes[labels[i]])
+    plt.show()
+
+
+    #4. Split dataset into train and test set
+    (train_X, test_X, train_Y, test_Y) = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+    train_Y = np_utils.to_categorical(train_Y, 3)
+
+    print(train_X.shape)
+    print(test_X.shape)
+    print(train_Y.shape)
+    print(test_Y.shape)
+
+
+    #5. Define model architecture
 
     # Creating classifier
     classifier = Sequential()
-    classifier.add(Conv2D(32, (3,3), input_shape=(32, 32, 3),
-                   activation = 'relu')) #recommended start with 64 kernels instead of 32 input_shape=(28, 28, 1)
 
-    classifier.add(BatchNormalization())
+    #Convolution layer
+    classifier.add(Convolution2D(32, (2, 2), activation='relu', input_shape=(HEIGHT, WIDTH, N_CHANNELS))) #64 layers is recomended
+    #classifier.add(BatchNormalization())
 
     #Pooling
-    classifier.add(MaxPooling2D(pool_size = (2,2)))
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
-    #Flattening
-    #classifier.add(Flatten())
+    #Convolution layer
+    classifier.add(Convolution2D(32, (2, 2), activation='relu'))
 
-    #Addition of one more convolution layer
-    classifier.add(Conv2D(32, (3,3), activation = 'relu'))
-    classifier.add(BatchNormalization())
-    classifier.add(MaxPooling2D(pool_size = (2,2)))
-
-    #Flattening
+    classifier.add(MaxPooling2D(pool_size=(2, 2)))
+    classifier.add(Dropout(0.25)) #Avoid overfitting
     classifier.add(Flatten())
 
-    #Dense Neural Net
-    classifier.add(Dense(units = 128, activation = 'relu'))
+    # Neural net
+    classifier.add(Dense(128, activation='relu'))
+    classifier.add(Dropout(0.5)) #Avoid overfitting
+    classifier.add(Dense(128, activation = 'relu'))
+    classifier.add(Dropout(0.5)) #Avoid overfitting
+    classifier.add(Dense(3, activation='softmax'))
 
-    classifier.add(Dropout(0.2)) #Avoid overfitting
+    classifier.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
 
-    classifier.add(Dense(units = 128, activation = 'relu')) #hidden layer
+    print(classifier.summary())
 
-    classifier.add(Dropout(0.2)) #Avoid overfitting
-
-    classifier.add(Dense(units = 100, activation = 'softmax')) #output layer
-    classifier.compile(loss = 'categorical_crossentropy',
-                       optimizer = 'adam', metrics = ['accuracy'])
-
-    predictors_training = np_utils.to_categorical(predictors_training, 100)
-    class_training = np_utils.to_categorical(class_training, 100)
-
-    classifier.fit(predictors_training, class_training,
-                   batch_size = 128, epochs = 5,
-                   validation_data = (predictors_test, class_test))
-
-    result = classifier.evaluate(predictors_test, class_test)
-    print(result)
+    #6. Fit classifier on training data
+    classifier.fit(train_X, train_Y, batch_size=32, epochs=25, verbose=1)
 
 
-# Press the green button in the gutter to run the script.
+    #7. Evaluate classifier on test data
+    pred = classifier.predict(test_X)
+    predictions = argmax(pred, axis=1) # return to label
+
+    cm = confusion_matrix(test_Y, predictions)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(cm)
+    plt.title('Model confusion matrix')
+    fig.colorbar(cax)
+    ax.set_xticklabels([''] + classes)
+    ax.set_yticklabels([''] + classes)
+
+    for i in range(3):
+    	for j in range(3):
+    		ax.text(i, j, cm[j, i], va='center', ha='center')
+
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+
+    accuracy = accuracy_score(test_Y, predictions)
+    print('Accuracy : %.2f%%' % (accuracy*100.0))
+
+
 if __name__ == '__main__':
     main()
